c11ab02b5310d6687c38c044a57b26ffd6c5fe98
Jose Sandoval <sandol1jose@gmail.com>
2021-08-20 17:14:45 -0600

    Video 14: Creacion de nuestros modelos

---
+++ Proyecto/Models/Conexion.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Models;
+
+use mysqli;
+
+class Conexion{
+        private $datos = array(
+            "host" => "localhost",
+            "user" => "root",
+            "pass" => "",
+            "db" => "proyecto"
+        );
+
+        private $con;
+
+        public function __construct(){
+            $this->con = new \mysqli(
+                $this->datos["host"], 
+                $this->datos["user"],
+                $this->datos["pass"],
+                $this->datos["db"],
+            );
+        }
+
+        public function consultaSimple($sql){
+            $this->con->query($sql);
+        }
+
+        public function consultaRetorno($sql){
+            $datos = $this->con->query($sql);
+            return $datos;
+        }
+
+    }
+
+?>
