a37622efcc36b65469e4921df07ac8410d32b873
Jose Sandoval <sandol1jose@gmail.com>
2021-08-20 18:05:54 -0600

    Video 15: Configuracion de Autoload

--- Proyecto/Models/Conexion.php
+++ Proyecto/Models/Conexion.php
@@ -1,6 +1,4 @@
-<?php
-
-namespace Models;
+<?php namespace Models;
 
 use mysqli;
 
--- Proyecto/Models/Estudiante.php
+++ Proyecto/Models/Estudiante.php
@@ -1,5 +1,4 @@
-<?php
-namespace Models;
+<?php namespace Models;
 
     class Estudiante{
         private $id;
@@ -12,7 +11,15 @@ namespace Models;
         private $con;
 
         public function __construct(){
-            $this->con = new Conexion;
+            $this->con = new Conexion();
+        }
+
+        public function set($atributo, $contenido){
+            $this->$atributo = $contenido;
+        }
+
+        public function get($atributo){
+            return $this->$atributo;
         }
 
         public function Listar(){
@@ -45,9 +52,10 @@ namespace Models;
 
         public function view(){
             $sql = "SELECT t1.*, t2.nombre as nombre_seccion FROM estudiantes t1 INNER JOIN secciones t2
-            ON t1.id_seccion = t2.id WHERE id = '{$this->id}'";
+            ON t1.id_seccion = t2.id WHERE t1.id = '{$this->id}'";
             $datos = $this->con->consultaRetorno($sql);
-            return $datos;
+            $row = mysqli_fetch_assoc($datos);
+            return $row;
         }
     }
 
